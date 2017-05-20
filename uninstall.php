de96a0251dc93ea2f87bf9febc5ee37e2277ceb6
scossar <scossar3@gmail.com>
2017-05-19 21:38:40 -0700

    Remove uninstall clean-up to allow subsequent versions of the plugin to be easily updated from zip file

--- uninstall.php
+++ uninstall.php
@@ -9,7 +9,10 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-delete_option( 'dcwl_groups' );
-delete_site_option( 'dcwl_groups' );
-
-delete_transient( 'wpdc_groups_data' );
+/**
+ * Todo: I'm removing the options clean-up calls for now so that users can easily update the plugin from a zip file.
+ * When I find a better way of approaching this I'll add back the following code:
+ * delete_option( 'dcwl_groups' );
+ * delete_site_option( 'dcwl_groups' );
+ * delete_transient( 'wpdc_groups_data' );
+ */
